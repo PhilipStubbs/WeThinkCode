2c1a5f0999b4175ae96f5a4c287ae343de12ed23
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-05-23 13:35:06 +0200

    fixes for memcmp

--- C/Projects/Libft 14-05-2018/ft_memcmp.c
+++ C/Projects/Libft 14-05-2018/ft_memcmp.c
@@ -14,11 +14,11 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char *p1;
-	char *p2;
+	unsigned char *p1;
+	unsigned char *p2;
 
-	p1 = (char*)s1;
-	p2 = (char*)s2;
+	p1 = (unsigned char*)s1;
+	p2 = (unsigned char*)s2;
 	while (n--)
 	{
 		if (*p1 == *p2)
@@ -27,7 +27,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			p2++;
 		}
 		else
-			return (*p1 - *p2);
+			return (*p1- *p2);
 	}
 	return (0);
 }
