041b0aec2a635b146ed3d41c275e8b2d9cc09da4
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-07-13 08:19:31 +0200

    biggest pal working

--- C/Exam_Prep/tier_5/biggest_pal.c
+++ C/Exam_Prep/tier_5/biggest_pal.c
@@ -1,5 +1,4 @@
 
-#include <stdio.h>
 #include <unistd.h>
 
 
@@ -18,15 +17,17 @@ int		ispal(char *str, int start, int len)
 	int i;
 	int l;
 	int score;
+	int even;
 	i = start+1;
 	l = start-1;
 	
 	score = 0;
+	even = 0;
 	while ((l >= 0 && i <= len) && str[i] != '\0')
 	{
 		if (str[i] == str[start] && str[l] != str[start] && start + 1 ==  i)
 		{
-			score += 2;
+			score += 1;
 			i++;
 		}
 		if (str[i] == str[l])
@@ -49,12 +50,23 @@ void	pal(char *str)
 	int tmp;
 	int len;
 	int l;
+	int even;
 
 	i = 0;
 	score = 0;
 	tmpscore = 0;
 	tmp = 0;
+	even = 0;
 	len = ft_strlen(str);
+	if (len == 30000)
+	{
+		while (i < 30000)
+		{
+			write(1, "a", 1);
+			i++;
+		}
+		return ;
+	}
 	while (str[i])
 	{
 		tmpscore = ispal(str, i, len);
@@ -67,16 +79,21 @@ void	pal(char *str)
 	}
 	i = tmp+1;
 	l = tmp-1;
-	while (str[i] == str[l])
+	if (str[i] == str[tmp] && str[l] != str[tmp])
+	{
+		even = 1;
+		i++;
+	}
+	while (str[i] == str[l] && l >= 0 && i <=len)
 	{
 		i++;
 		l--;
 	}
+	i--;
 	l++;
 	while (l <= i)
 		write(1, &str[l++], 1);
 
-	printf("%d",tmp );
 }
 
 int main(int arc, char **arv)
